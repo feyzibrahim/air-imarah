c17ddc0360d1047421c53535d7ea0d967c19afac
Ahalya-KT <ahalyakt002@gmail.com>
2025-06-01 20:54:23 +0530

    set up project

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "../Components/ui/button"
+
+function Navbar() {
+  return (
+    <div>
+         <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Navbar
